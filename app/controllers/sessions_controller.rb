243d885246407fdd013ca9317449a8a9443daf78
Kevin Cai <kevincai1979@gmail.com>
2018-01-17 01:32:57 -0800

    add log out function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,5 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
   end
 end
