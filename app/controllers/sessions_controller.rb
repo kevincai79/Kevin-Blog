723ade3fa6e6a61e203967cb9743dafc0d759229
Kevin Cai <kevincai1979@gmail.com>
2018-01-17 01:24:47 -0800

    add log in function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Log in information was not correct"
+      render 'new'
+    end
   end
 
-  def destroy 
+  def destroy
   end
 end
