ac085c51e7694077f78ee7daafd7740eacf7abf0
Kevin Cai <kevincai1979@gmail.com>
2018-01-25 14:43:54 -0800

    add comment restrictions on controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def new
     @article = Article.find(params[:article_id])
     @article_comment = @article.comments.new
@@ -16,7 +20,25 @@ class CommentsController < ApplicationController
   end
 
   private
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You need to log in to perform that action"
+      redirect_to root_path
+    end
+  end
+
+  def require_same_user
+    if current_user.id != @comment.user_id && !current_user.admin?
+      flash[:danger] = "You can only edit or delete your own comments"
+      redirect_to root_path
+    end
   end
 end
