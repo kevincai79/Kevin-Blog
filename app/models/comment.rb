576fe6b6a9c239aa1d1e36ad1b58a93e641fe395
Kevin Cai <kevincai1979@gmail.com>
2018-01-24 16:23:38 -0800

    set up comment table, model and controller

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ApplicationRecord
+  belongs_to :article
+
+  validates :content, presence: true, length: {minimum: 5, maximum: 100}
+  validates :article_id, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124023934) do
+ActiveRecord::Schema.define(version: 20180125000403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20180124023934) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
